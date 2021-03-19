9588033ba4c31f21c2e8c93bf6f87cd23401ae40
Siobhan <siobhan@robotical.io>
2021-03-19 13:30:10 +0000

    parked this fix

--- src/components/language-selector/language-selector.jsx
+++ src/components/language-selector/language-selector.jsx
@@ -2,10 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import locales from 'scratch-l10n';
+// import locales from '../marty-l10n/supported-locales.js';
 import styles from './language-selector.css';
 
 // supported languages to exclude from the menu, but allow as a URL option
 const ignore = [];
+
+// console.log(locales)
 
 const LanguageSelector = ({currentLocale, label, onChange}) => (
     <select
